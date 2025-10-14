e959ab0a88cade6e1e4db9a08400be552d6a89ff
IzharMohammed <izharmohammed21@gmail.com>
2025-10-14 22:39:29 +0530

    Feat:env config

---
+++ constants/config.ts
@@ -0,0 +1,18 @@
+
+interface ApiConfig {
+    baseURL: string;
+    apiKey: string;
+    endpoints: {
+        signin: string;
+        signup: string;
+    };
+}
+
+export const API_CONFIG: ApiConfig = {
+    baseURL: process.env.EXPO_PUBLIC_BACKEND_URL || 'http://localhost:9999',
+    apiKey: process.env.EXPO_PUBLIC_BACKEND_API_KEY || '',
+    endpoints: {
+        signin: '/v1/signin',
+        signup: '/v1/signup',
+    }
+}
